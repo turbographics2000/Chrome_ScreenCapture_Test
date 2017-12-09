d5eb627c19e66584f4832163d6685dbec05de313
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-12-09 18:03:45 +0900

    commit

--- extension/bg.js
+++ extension/bg.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((msg, sender, res) => {
     if (msg.ready) {
-        chrome.desktopCapture.chooseDesktopMedia(['screen', 'window', 'tab', 'audio'], sender.tab, streamId => {
+        chrome.desktopCapture.chooseDesktopMedia(['screen', 'window', 'tab'], sender.tab, streamId => {
             chrome.tabs.sendMessage(sender.tab.id, { streamId });
         });
     }
